1a6a4c1612752c126fb059ab3da08ea72dfd7eed
LUXURYGIT <luxurybag.india@gmail.com>
2024-11-23 11:00:07 +0530

    Initial commit

--- js/script.js
+++ js/script.js
@@ -122,24 +122,24 @@ document.addEventListener("DOMContentLoaded", function(){
 // adding funtionality to back to top button 
 
 //Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+// let mybutton = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+// function scrollFunction() {
+//   if (
+//     document.body.scrollTop > 20 ||
+//     document.documentElement.scrollTop > 20
+//   ) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 // When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click",function(){
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-});
+// mybutton.addEventListener("click",function(){
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// });
